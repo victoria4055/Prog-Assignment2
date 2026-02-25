29ee55090adad626c22157f609c5c8d8b7437cd6
victoria4055 <victoria.villasa@ufl.edu>
2026-02-25 13:33:41 -0500

    Added optff eviction function

--- src/main.py
+++ src/main.py
@@ -61,12 +61,59 @@ def lru(k, requests):
 
     return pfs
 
+def optff(k, requests):
+    frames = []
+    pfs = 0
+    n = len(requests)
+
+    # if its a hit
+    for i, request in enumerate(requests):
+        if request in frames:
+            continue
+
+        # checking if its a miss
+        pfs += 1
+
+        # empty slot if avail
+        if len(frames) < k:
+            frames.append(request)
+            continue
+
+        # if the cache is full
+        # have to get frame who their next use is farthest away in future
+        chosen_frame = None
+        farthest_frame = -1
+
+        for idx, page in enumerate(frames):
+            used_next = None
+            for j in range(i + 1, n):
+                if requests[j] == page:
+                    used_next = j
+                    break
+
+            # if its not used again then we can evict it
+            if used_next is None:
+                chosen_frame = idx
+                farthest_frame = n + 1
+                break
+
+            # if not then keep the one used farthest in the future
+            if used_next > farthest_frame:
+                farthest_frame = used_next
+                chosen_frame = idx
+
+        # now exict the chosen frame and add the new request
+        frames.pop(chosen_frame)
+        frames.append(request)
+    return pfs
+
 def main():
-    filename = "ProgAssignment2/testfiles/ex3.txt"
+    filename = "testfiles/ex1.txt"
 
     k, requests = read_file(filename)
     print(f"FIFO: {fifo(k,requests)}")
     print(f"LRU: {lru(k,requests)}")
+    print(f"OPTFF: {optff(k,requests)}")
 
 
 
